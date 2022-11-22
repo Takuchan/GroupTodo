4d679e6947566f4c6c7846234923454ac6b159e5
Takuchan <takuchanapp@gmail.com>
2022-11-22 23:33:29 +0900

    ホーム画面作成

---
+++ src/EnterRoom.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./App.css";
+import CreateTodo from './CreateTodo';
+import Home from './Home';
+//Screen To
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+
+
+export const EnterRoom = () => {
+  return (
+    <div>
+        <h1>re</h1>
+    </div>
+    
+ 
+  )
+}
+
+export default EnterRoom
---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div>
+          <div className="card">
+                <h4><a href="https://takuchanwork.web.app/index.html">Go to Takuchan Profile</a></h4>
+                <div className='content'>
+                <h1>Group Todo</h1>
+                <input type="name" placeholder="room seacret word.."/>
+                
+                <div className='submitRoomKey'><a herf="index.html">EnterRoom</a></div>
+                <a href="#">create a seacret group ToDo</a>
+
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default Home
